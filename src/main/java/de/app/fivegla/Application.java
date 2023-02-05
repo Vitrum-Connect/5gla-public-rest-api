8df9c73229c8d7915c67cb6969b26f0853bd8277
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-02-05 11:17:27 +0100

    Update OpenAPI description.

--- src/main/java/de/app/fivegla/Application.java
+++ src/main/java/de/app/fivegla/Application.java
@@ -1,5 +1,6 @@
 package de.app.fivegla;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,6 +10,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @EnableWebMvc
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "5GLA REST API",
+                version = "1.0",
+                description = "REST API for the 5GLA environment. This API ensures, that the sensors can be registered and the data can be sent to the 5GLA environment."
+        ),
+        servers = @io.swagger.v3.oas.annotations.servers.Server(
+                url = "---",
+                description = "Currently not available."
+        )
+)
 public class Application {
 
     public static void main(String[] args) {

d8d2e39a52d8774928cf55729403eaebc595579d
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-04-03 11:20:59 +0200

    Suppress warning.

--- src/main/java/de/app/fivegla/persistence/ApplicationData.java
+++ src/main/java/de/app/fivegla/persistence/ApplicationData.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 public class ApplicationData {
 
     @Autowired
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
     private transient StorageManager storageManager;
 
     @Getter

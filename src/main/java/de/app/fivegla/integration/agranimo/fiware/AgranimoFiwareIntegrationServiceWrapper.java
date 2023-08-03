62bb504ad97470401e06eb6c5d1910a301c10ad8
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-08-03 13:33:04 +0200

    Ignore unused services.

--- src/main/java/de/app/fivegla/integration/agranimo/fiware/AgranimoFiwareIntegrationServiceWrapper.java
+++ src/main/java/de/app/fivegla/integration/agranimo/fiware/AgranimoFiwareIntegrationServiceWrapper.java
@@ -14,6 +14,7 @@ import java.util.UUID;
  */
 @Slf4j
 @Service
+@SuppressWarnings({"FieldCanBeLocal", "unused"})
 public class AgranimoFiwareIntegrationServiceWrapper {
     private final DeviceIntegrationService deviceIntegrationService;
     private final DeviceMeasurementIntegrationService deviceMeasurementIntegrationService;

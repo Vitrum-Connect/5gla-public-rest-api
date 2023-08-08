c8d59d0655e7f140704256e1231f05c997d3c98f
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-08-08 13:36:44 +0200

    Resolve code problems.

--- src/test/java/de/app/fivegla/SpringBootIntegrationTestBase.java
+++ src/test/java/de/app/fivegla/SpringBootIntegrationTestBase.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 public class SpringBootIntegrationTestBase {
 
     @MockBean
+    @SuppressWarnings("unused")
     private CollectorRegistry collectorRegistry;
 
 }

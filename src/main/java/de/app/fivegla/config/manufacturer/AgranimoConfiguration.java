c94f7406bcbafa15a3e97a5fb19a4be89f803d64
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-08-17 20:32:08 +0200

    "Refactor AgranimoConfiguration comments for readability
    Unnecessary comment lines in the AgranimoConfiguration.java file were deleted. This step is taken to improve the readability and maintainability of the code, keeping the core functionality unchanged."

--- src/main/java/de/app/fivegla/config/manufacturer/AgranimoConfiguration.java
+++ src/main/java/de/app/fivegla/config/manufacturer/AgranimoConfiguration.java
@@ -4,11 +4,9 @@ import de.app.fivegla.api.Manufacturer;
 
 /**
  * Represents the configuration for the Agranimo module.
- *
  * This class provides the necessary properties to configure the Agranimo module,
  * such as enablement status, manufacturer, device ID prefix, measurement ID prefix,
  * username, password, and URL.
- *
  * The AgranimoConfiguration class also implements the CommonManufacturerConfiguration
  * interface which provides the manufacturerName() method to retrieve the lowercase
  * name of the manufacturer.

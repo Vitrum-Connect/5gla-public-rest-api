bbde2ae6acb4155b06d6866760d03fd0ffaf4082
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-08-17 20:32:21 +0200

    "Refactor AgranimoConfiguration comments for readability
    Unnecessary comment lines in the AgranimoConfiguration.java file were deleted. This step is taken to improve the readability and maintainability of the code, keeping the core functionality unchanged."

--- src/main/java/de/app/fivegla/config/manufacturer/Farm21Configuration.java
+++ src/main/java/de/app/fivegla/config/manufacturer/Farm21Configuration.java
@@ -6,7 +6,6 @@ import de.app.fivegla.api.Manufacturer;
  * Represents the configuration for a Farm21 device.
  * It includes information such as whether the device is enabled, the manufacturer,
  * the device ID prefix, measurement ID prefix, authentication credentials, and API related information.
- *
  * This class implements the {@link CommonManufacturerConfiguration} interface and provides the ability
  * to retrieve the manufacturer name in lower case.
  */

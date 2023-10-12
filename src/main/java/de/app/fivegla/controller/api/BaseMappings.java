0733a334c40e539b08e049dede8266e1599f0ee5
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-10-12 13:42:00 +0200

    Feature/remove duplicate api prefix from path (#80)

--- src/main/java/de/app/fivegla/controller/api/BaseMappings.java
+++ src/main/java/de/app/fivegla/controller/api/BaseMappings.java
@@ -2,9 +2,8 @@ package de.app.fivegla.controller.api;
 
 public class BaseMappings {
 
-    public static final String API_V1 = "/api/v1";
+    public static final String API_V1 = "/v1";
     public static final String MICA_SENSE = API_V1 + "/mica-sense";
-    public static final String MANUAL_IMPORT = API_V1 + "/manual-import";
     public static final String DATA_LOGGING = API_V1 + "/data-logging";
     public static final String MAINTENANCE = API_V1 + "/maintenance";
     public static final String INFO = API_V1 + "/info";

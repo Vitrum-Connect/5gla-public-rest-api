bc612fe4d3470005c879131ee951fc2406514fa0
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-10-24 11:05:10 +0200

    Update DeviceRegistrationController and add DeviceRegistrationRequest class
    Included DeviceRegistrationRequest object in DeviceRegistrationController's register method to capture registration details. A new DeviceRegistrationRequest DTO is introduced explicitly for a device registration process to accept the manufacturer, ID and location details (latitude and longitude) of the device.
    
    Adjusted DeviceRegistrationController to use this DTO in its register method. This improvement makes the device registration more flexible, maintaining the separation of concerns and cleaner handling of registration details. It also improves readability and maintainability of the code.

--- src/main/java/de/app/fivegla/controller/DeviceRegistrationController.java
+++ src/main/java/de/app/fivegla/controller/DeviceRegistrationController.java
@@ -2,11 +2,14 @@ package de.app.fivegla.controller;
 
 import de.app.fivegla.controller.api.BaseMappings;
 import de.app.fivegla.controller.api.swagger.OperationTags;
+import de.app.fivegla.controller.dto.request.DeviceRegistrationRequest;
+import de.app.fivegla.integration.generic.GenericDeviceIntegrationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(BaseMappings.DEVICE_REGISTRATION)
 public class DeviceRegistrationController {
+
+    private final GenericDeviceIntegrationService genericDeviceIntegrationService;
+
+    public DeviceRegistrationController(GenericDeviceIntegrationService genericDeviceIntegrationService) {
+        this.genericDeviceIntegrationService = genericDeviceIntegrationService;
+    }
 
     @Operation(
             operationId = "device-registration.register",
@@ -32,7 +41,8 @@ public class DeviceRegistrationController {
             description = "The request is invalid."
     )
     @PostMapping
-    public ResponseEntity<Void> register() {
+    public ResponseEntity<Void> register(@RequestBody DeviceRegistrationRequest request) {
+        genericDeviceIntegrationService.register(request.getManufacturer(), request.getId(), request.getLatitude(), request.getLongitude());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 

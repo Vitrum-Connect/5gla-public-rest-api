08dab829c59ff1e8929d45d4e6296adcc91b9156
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-02-28 17:18:40 +0100

    Add AGVOLUTION_SENSOR and UNKNOWN to MeasurementType enum
    Additional sensor types AGVOLUTION_SENSOR and UNKNOWN have been added to the MeasurementType enum. Concurrently, refactoring took place in the devices' data persistence, which uses now the DeviceMeasurementBuilder. The obsolete FiwareIdGenerator class has been removed in this process.

--- src/main/java/de/app/fivegla/integration/agvolution/AgvolutionFiwareIntegrationServiceWrapper.java
+++ src/main/java/de/app/fivegla/integration/agvolution/AgvolutionFiwareIntegrationServiceWrapper.java
@@ -1,13 +1,13 @@
 package de.app.fivegla.integration.agvolution;
 
 
-import de.app.fivegla.api.FiwareIdGenerator;
-import de.app.fivegla.api.Format;
+import de.app.fivegla.api.enums.MeasurementType;
 import de.app.fivegla.config.ApplicationConfiguration;
 import de.app.fivegla.config.manufacturer.CommonManufacturerConfiguration;
 import de.app.fivegla.fiware.DeviceMeasurementIntegrationService;
+import de.app.fivegla.fiware.api.FiwareTypes;
 import de.app.fivegla.fiware.model.DeviceMeasurement;
-import de.app.fivegla.fiware.model.Location;
+import de.app.fivegla.fiware.model.builder.DeviceMeasurementBuilder;
 import de.app.fivegla.integration.agvolution.model.SeriesEntry;
 import de.app.fivegla.integration.agvolution.model.TimeSeriesEntry;
 import lombok.RequiredArgsConstructor;
@@ -44,16 +44,15 @@ public class AgvolutionFiwareIntegrationServiceWrapper {
         log.debug("Persisting data: {}", timeSeriesEntry);
         var deviceMeasurements = new ArrayList<DeviceMeasurement>();
         timeSeriesEntry.getValues().forEach(timeSeriesValue -> {
-            var deviceMeasurement = DeviceMeasurement.builder()
-                    .id(FiwareIdGenerator.create(getManufacturerConfiguration(), seriesEntry.getDeviceId()))
-                    .manufacturerSpecificId(seriesEntry.getDeviceId())
-                    .dateObserved(Format.format(timeSeriesValue.getTime()))
-                    .location(Location.builder()
-                            .coordinates(List.of(seriesEntry.getLatitude(), seriesEntry.getLongitude()))
-                            .build())
-                    .controlledProperty(timeSeriesEntry.getKey())
-                    .numValue(timeSeriesValue.getValue())
-                    .unit(timeSeriesEntry.getUnit())
+            var deviceMeasurement = new DeviceMeasurementBuilder()
+                    .withId(getManufacturerConfiguration() + ":" + seriesEntry.getDeviceId())
+                    .withType(MeasurementType.AGVOLUTION_SENSOR.name())
+                    .withLocation(seriesEntry.getLatitude(), seriesEntry.getLongitude())
+                    .withMeasurement(timeSeriesEntry.getKey(),
+                            FiwareTypes.TEXT.getKey(),
+                            String.valueOf(timeSeriesValue.getValue()),
+                            timeSeriesValue.getTime(),
+                            timeSeriesEntry.getKey())
                     .build();
             deviceMeasurements.add(deviceMeasurement);
         });

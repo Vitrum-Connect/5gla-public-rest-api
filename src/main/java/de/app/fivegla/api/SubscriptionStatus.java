29a8ae30faf4624aac9b9456a0d7135ca4e639a7
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-04-29 10:40:12 +0200

    Improve subscription handling per tenant (#170)

--- src/main/java/de/app/fivegla/api/SubscriptionStatus.java
+++ src/main/java/de/app/fivegla/api/SubscriptionStatus.java
@@ -1,16 +1,16 @@
 package de.app.fivegla.api;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
 
 /**
  * This class represents the subscription status of a service or application.
  * It provides methods to get and set the status of subscriptions.
  */
-@Setter
 @Component
 @Scope("singleton")
 public class SubscriptionStatus {
@@ -19,15 +19,25 @@ public class SubscriptionStatus {
     @Value("${app.fiware.subscriptions.enabled}")
     private boolean subscriptionsEnabled;
 
-    private boolean subscriptionsSent;
+    private final HashMap<String, Boolean> subscriptionsSent = new HashMap<>();
 
     /**
      * Checks if subscriptions are enabled and if subscriptions have not been sent yet.
      *
      * @return true if subscriptions are enabled and subscriptions have not been sent yet, false otherwise
      */
-    public boolean sendOutSubscriptions() {
-        return subscriptionsEnabled && !subscriptionsSent;
+    public boolean sendOutSubscriptions(String tenantId) {
+        return subscriptionsEnabled && subscriptionsSent.get(tenantId) != null && !subscriptionsSent.get(tenantId);
+    }
+
+    /**
+     * Sets the status of subscriptions for a specific tenant.
+     *
+     * @param tenantId The ID of the tenant for which the status of subscriptions is being set.
+     */
+    public void subscriptionSent(String tenantId) {
+        subscriptionsSent.put(tenantId, true);
+
     }
 
 }

f64831fc3e6b8ad18fa18c42aa89159826ae37a3
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-04-30 14:05:57 +0200

    Update Javadoc format in GpsCoordinate.java
    The Javadoc for GPS coordinates related fields in 'GpsCoordinate.java' were updated. The changes include replacing the simple asterisks with paragraph tags to clearly denote the new paragraph, improving code readability.

--- src/main/java/de/app/fivegla/business/agricrop/GpsCoordinate.java
+++ src/main/java/de/app/fivegla/business/agricrop/GpsCoordinate.java
@@ -9,14 +9,14 @@ public class GpsCoordinate {
 
     /**
      * Represents the latitude coordinate of a GPS location.
-     *
+     * <p>
      * The latitude value is stored as a double precision floating point number.
      */
     private double latitude;
 
     /**
      * Represents the longitude coordinate of a GPS location.
-     *
+     * <p>
      * The longitude value is stored as a double precision floating point number.
      */
     private double longitude;

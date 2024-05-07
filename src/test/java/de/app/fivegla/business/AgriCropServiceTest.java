311a47aefa668847921cd32cfa50fd5cc567c1b9
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-05-07 15:26:45 +0200

    Refactor AgriCropServiceTest and remove unused code
    Unused 'feature' variable was removed from 'AgriCropServiceTest'. Furthermore, setup and teardown methods were implemented using '@BeforeEach' and '@AfterEach' annotations, replacing the constructor. These changes improve test structure and make it more identifiable.

--- src/test/java/de/app/fivegla/business/AgriCropServiceTest.java
+++ src/test/java/de/app/fivegla/business/AgriCropServiceTest.java
@@ -2,56 +2,15 @@ package de.app.fivegla.business;
 
 import de.app.fivegla.integration.agricrop.AgriCropFiwareIntegrationServiceWrapper;
 import de.app.fivegla.persistence.entity.Tenant;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 class AgriCropServiceTest {
 
-
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String feature = """
-            {
-                "type": "FeatureCollection",
-                "features": [
-                  {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                      "coordinates": [
-                        [
-                          [
-                            10.443273285881673,
-                            52.88334609790465
-                          ],
-                          [
-                            10.437440177134079,
-                            52.87646841445812
-                          ],
-                          [
-                            10.440236095438934,
-                            52.873898638877165
-                          ],
-                          [
-                            10.443741637678897,
-                            52.87545765422925
-                          ],
-                          [
-                            10.446778828121694,
-                            52.87920077546457
-                          ],
-                          [
-                            10.443273285881673,
-                            52.88334609790465
-                          ]
-                        ]
-                      ],
-                      "type": "Polygon"
-                    }
-                  }
-                ]
-              }""";
 
     @SuppressWarnings("FieldCanBeLocal")
     private final String csv = """
@@ -71,10 +30,15 @@ class AgriCropServiceTest {
     @Mock
     private AgriCropFiwareIntegrationServiceWrapper agriCropFiwareIntegrationServiceWrapper;
 
-
-    public AgriCropServiceTest() {
+    @BeforeEach
+    public void setup() {
         openMocks = MockitoAnnotations.openMocks(this);
         this.agriCropService = new AgriCropService(agriCropFiwareIntegrationServiceWrapper);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        openMocks.close();
     }
 
     @Test

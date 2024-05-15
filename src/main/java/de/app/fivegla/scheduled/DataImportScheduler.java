c10ce2e56869af5e3ad41beb21e63f47bb5fec4e
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-05-15 08:26:33 +0200

    Add empty tenant check in scheduleDataImport method
    Added a check to ensure tenants exist before proceeding with the scheduled data import in the DataImportScheduler class. If no tenants are found, the method will log the occurrence and skip the data import, preventing unnecessary operations.

--- src/main/java/de/app/fivegla/scheduled/DataImportScheduler.java
+++ src/main/java/de/app/fivegla/scheduled/DataImportScheduler.java
@@ -3,11 +3,14 @@ package de.app.fivegla.scheduled;
 import de.app.fivegla.business.TenantService;
 import de.app.fivegla.business.ThirdPartyApiConfigurationService;
 import de.app.fivegla.event.events.DataImportEvent;
+import de.app.fivegla.persistence.entity.Tenant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Scheduled data import for all manufacturers.
@@ -27,13 +30,18 @@ public class DataImportScheduler {
     @Scheduled(initialDelayString = "${app.scheduled.data-import.initial-delay}", fixedDelayString = "${app.scheduled.data-import.delay}")
     public void scheduleDataImport() {
         log.info("Scheduled data import started for all third-party APIs.");
-        tenantService.findAll().forEach(tenant -> thirdPartyApiConfigurationService.getThirdPartyApiConfigurations(tenant.getTenantId()).forEach(configuration -> {
-            if (configuration.isEnabled()) {
-                applicationEventPublisher.publishEvent(new DataImportEvent(configuration));
-            } else {
-                log.info("Skipping data import for tenant {} and manufacturer {} because it is disabled.", tenant.getName(), configuration.getManufacturer());
-            }
-        }));
+        List<Tenant> tenants = tenantService.findAll();
+        if (null == tenants || tenants.isEmpty()) {
+            log.info("No tenants found. Skipping data import.");
+        } else {
+            tenants.forEach(tenant -> thirdPartyApiConfigurationService.getThirdPartyApiConfigurations(tenant.getTenantId()).forEach(configuration -> {
+                if (configuration.isEnabled()) {
+                    applicationEventPublisher.publishEvent(new DataImportEvent(configuration));
+                } else {
+                    log.info("Skipping data import for tenant {} and manufacturer {} because it is disabled.", tenant.getName(), configuration.getManufacturer());
+                }
+            }));
+        }
     }
 
 }

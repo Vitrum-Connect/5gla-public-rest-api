2132389afca5eb809c92b554dd9234652acf63c9
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-05-30 18:00:44 +0200

    Remove creation and update timestamps from group data
    Removed 'createdAt' and 'updatedAt' attributes from the Group class and related methods in the GroupController. Changes have been made to ensure clean and precise group data representation without unnecessary timestamps detailing group's lifecycle events. The java.time.Instant import was also removed from the GroupService and TenantService classes as it became unnecessary.

--- src/main/java/de/app/fivegla/business/TenantService.java
+++ src/main/java/de/app/fivegla/business/TenantService.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
